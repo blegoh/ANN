1a41ccc9f8cad0f4e175cc77e9fddc82e791b052
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2018-03-02 02:21:16 +0700

    add Ann Class

---
+++ Ann.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yusuf
+ * Date: 02/03/18
+ * Time: 2:02
+ */
+
+class Ann
+{
+
+    private $learningRate;
+
+    /**
+     * Ann constructor.
+     */
+    public function __construct()
+    {
+        $this->learningRate = 0.01;
+    }
+
+    /**
+     * @param array $input
+     */
+    private  function transpose(Array $input)
+    {
+        $transpose = [];
+        foreach ($input as $key => $item) {
+            foreach ($item as $k => $i) {
+                
+            }
+        }
+    }
+
+
+}
